4a4c9e34844ad4fa4ca93c810ed23ca3b8b48865
chilty <eho090917@gmail.com>
2024-08-31 11:02:52 +0900

    20240831

--- apps/app.py
+++ apps/app.py
@@ -1,7 +1,27 @@
 from flask import Flask
+from pathlib import Path
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
+
+db=SQLAlchemy()
+csrf = CSRFProtect()
 
 def create_app():
     app = Flask(__name__)
+    #앱의 config 설정
+    app.config.from_mapping(
+            SECRET_KEY = "2AZSMss3p5QPbcY2hBsJ",
+            SQLALCHEMY_DATABASE_URI =f"sqlite:///{Path(__file__).parent.parent/'local.sqlite'}", 
+            SQLALCHEMY_TRACK_MODIFICATIONS=False,
+            SQLAlCHEMY_ECHO=True,
+            WTF_CSRF_SECRET_KEY='AuwzyszU5sugKN7Ks6f'
+            )     
+    # SQLAlchemy와 앱을 연계한
+    db.init_app(app) 
+    # Migrate와 앱을 연계한다
+    Migrate(app, db)
+    csrf.init_app(app)
 
     from apps.crud import views as crud_views
     app.register_blueprint(crud_views.crud,url_prefix="/crud")
--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,4 +1,7 @@
 from flask import Flask, Blueprint, render_template
+from apps.app import db
+from apps.crud.models import User
+from apps.crud.forms import UserForm
 
 crud=Blueprint(
     "crud", __name__,
@@ -9,3 +12,33 @@ crud=Blueprint(
 @crud.route("/")
 def index():
     return render_template("crud/index.html")
+@crud.route("/sql")
+def sql():
+    '''user=User(
+        username= '김태우',
+        email='eho090917@gmail.com',
+        userid='chilty',
+        password_hash='1111'
+    )
+    db.session.add(user)
+    db.session.commit()
+    db.session.query(User).all()
+    db.session.query(User).filter(id=1).all()'''
+    db.session.query(User).filter(id=1).delete()
+    db.session.commit()
+    return '콘솔 로그 확인'
+
+@crud.route('/users/new',methods=["GET","POST"])
+def create_user():
+    form=UserForm()
+    if form.validate_on_submit():
+        user=User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for("crud.users"))
+    return render_template("crud/create.html",form=form)
